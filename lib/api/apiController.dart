02b383884d128e14554a1f1c9dd4358b00f99c8d
nputra101104-dotcom <nputra101104@gmail.com>
2026-06-21 13:29:58 +0700

    Delete lib directory

--- lib/api/apiController.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-
-class Apicontroller {
-  Future<List<dynamic>> getdatas() async {
-    final response = await Dio().get('https://ghibliapi.vercel.app/films');
-    return response.data;
-  }
-}
